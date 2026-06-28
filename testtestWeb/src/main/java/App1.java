5c0600026ec32e56d58499e83eb4fd7a93840d48
kitoumasaomi <kanfutrooper@gmail.com>
2026-06-28 15:43:54 +0900

    App1.java作成

--- testtestWeb/src/main/java/App1.java
+++ testtestWeb/src/main/java/App1.java
@@ -17,11 +17,24 @@ public class App1 extends HttpServlet {
         String param3 = request.getParameter("param3");
 
         //HTMLを作成する処理
-        String html = "<html><body><h1>Hello, World!</h1></body></html>";
+        String html = "<html>"
+        + "<body>"
+        + パラメータ名: param1 値: " + param1 + "<br/>"
+        + パラメータ名: param2 値: " + param2 + "<br/>"
+        + パラメータ名: param3 値: " + param3 + "<br/>"
+        + "</body>"
+        + "</html>";
+
+        //レスポンスのヘッダーに、MINEタイプ、文字コードをセット
+        response.setContentType("text/html; charset=UTF-8");
+        response.getCharacterEncoding("UTF-8");
+
+        //作成したHTMLを出力
         response.getWriter().println(html);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
     }
 }
