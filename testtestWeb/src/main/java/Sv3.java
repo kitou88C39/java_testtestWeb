e8133505f4ddac113ca9b6d65bcb0757e662e9cf
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-09 20:29:56 +0900

    Sv3.javaを作成

--- testtestWeb/src/main/java/Sv3.java
+++ testtestWeb/src/main/java/Sv3.java
@@ -4,24 +4,25 @@ public class Sv2 extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-//JSPへフォワード
+//検索条件のJSPにフォワード
     ServletContext sc = getServletContext();
     RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/sv2_in.jsp");
-    rd.forward(req,resp
+    rd.forward(req,resp);
 
 @Override
 protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-    //入力内容(文字列)を取得
-    String str = req.getParameter("str");
+    //パラメータ(キーワード)を取得
+    String kw=req.getParameter("kw");
 
-    //文字列を加工
-    Editor e = new Editor();
-    String editStr = e.execute(str);
+    //検索のクラスでキーワードを検索
+    Kensaku e = new Kensaku();
+    List<String> resultList=k.execute(kw);
 
-    //リクエストへ加工した文字列をセット
-    req.setAttribute("result", editStr);
+    //キーワードと検索結果をリクエストにセット
+    req.setAttribute("keyword", kw);
+    req.setAttribute("result", resultList);
 
     //JSPへフォワード
     ServletContext sc = getServletContext();
