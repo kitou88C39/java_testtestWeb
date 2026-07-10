a738f5c8885be7b228a35adc5561fac725ecd9aa
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-10 20:47:54 +0900

    Kensaku.javaを作成

---
+++ testtestWeb/src/main/java/Kensaku.java
@@ -0,0 +1,26 @@
+public class Kensaku {
+
+    public List<String> execute(String kw){
+
+    ArrayList<String> resultList = new ArrayList<String>();
+
+    try{
+    //ファイルの読み込み
+    List<String> dataList = loadFile();
+    //キーワードを含む行を検索
+    for(String s : dataList){
+        if(s.indexOf(kw) != -1){
+            resultList.add(s);
+        }
+    } catch(Exception e){
+    return null;
+    }
+    return resultList;
+    }
+
+    private List<String> loadFile() throws FileFoundException, FileFoundException{
+
+    }
+
+}
+}
