cbc6bc5f52a1347cd5c651d11c8172889535d9f9
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-16 15:20:59 +0900

    Sv23.javaを作成

---
+++ testtestWeb/src/main/webapp/WEB-INF/Sv23.java
@@ -0,0 +1,33 @@
+public class Sv23 extends HttpServlet {
+
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+//検索条件のJSPにフォワード
+    ServletContext sc = getServletContext();
+    RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/sv2_in.jsp");
+    rd.forward(req,resp);
+
+@Override
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    //パラメータ(キーワード)を取得
+    String kw=req.getParameter("kw");
+
+    //検索のクラスでキーワードを検索
+    Kensaku e = new Kensaku();
+    List<String> resultList=k.execute(kw);
+
+    //キーワードと検索結果をリクエストにセット
+    req.setAttribute("keyword", kw);
+    req.setAttribute("result", resultList);
+
+    //検索条件のJSPへフォワード
+    ServletContext sc = getServletContext();
+    RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/sv3.jsp");
+    rd.forward(req,resp);
+    }
+  }
+}
