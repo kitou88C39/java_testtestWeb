5b6e97ccdd88e9594edb363e859e8a46ac2441a4
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-17 11:05:20 +0900

    Sv25.javaを作成

--- testtestWeb/src/main/java/Sv25.java
+++ testtestWeb/src/main/java/Sv25.java
@@ -1,20 +1,18 @@
-public class Sv24 extends HttpServlet {
+public class Sv25 extends HttpServlet {
 
 @Override
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-    //都道府県名の一覧を取得
-    Kensaku e = new Kensaku();
-    K.execute();
-
-    ArrayList<Todofuken> todofukenList = k.getTodofukenList();
-
-    //結果をリクエストにセットして、jspにフォワード
-    req.setAttribute("todofuken_list", todofukenList);
-
+    //検索条件を表示
     ServletContext sc = getServletContext();
     RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/jsp/out.jsp");
     rd.forward(req,resp);
     }
+
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
   }
+ }
