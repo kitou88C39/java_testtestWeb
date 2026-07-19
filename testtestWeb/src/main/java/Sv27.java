9fb1122c0e26f433368849be97856904336ce5b5
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-19 11:18:04 +0900

    Sv27.javaを作成

--- testtestWeb/src/main/java/Sv27.java
+++ testtestWeb/src/main/java/Sv27.java
@@ -4,15 +4,36 @@ public class Sv27 extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-    //検索条件を表示
+    //データ追加ページを表示
     ServletContext sc = getServletContext();
     RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/jsp/out.jsp");
     rd.forward(req,resp);
     }
 
 @Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+    //入力パラメータを取得
+    String kenCode = req.getParameter("txtKenCode");
+    String kenName = req.getParameter("txtKenName");
+    String yomigana = req.getParameter("txtYomigana");
+
+    //データの追加
+    Tuika t = new Tuika();
+    t.execute(kenCode, kenName, yomigana);
+
+    int updateRowa = t.getUpdateRows();
+
+    //結果をリクエストにセット
+    req.setAttribute("ken_code", kenCode);
+    req.setAttribute("ken_name", kenName);
+    req.setAttribute("yomigana", yomigana);
+    req.setAttribute("update_Rows", updateRows);
+
+    //検索条件を表示
+    ServletContext sc = getServletContext();
+    RequestDispatcher rd = sc.getRequestDispatcher("/WEB-INF/jsp/out.jsp");
+    rd.forward(req,resp);
+    }
   }
- }
