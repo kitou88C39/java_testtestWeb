67d8ae84ceca28c959facb761bb31357ca13c4cb
kitoumasaomi <kanfutrooper@gmail.com>
2026-07-19 21:27:18 +0900

    Kensaku29.javaを作成

--- testtestWeb/src/main/java/Kensaku29.java
+++ testtestWeb/src/main/java/Kensaku29.java
@@ -30,19 +30,31 @@ public void execute(String kenName) throws Exception {
 
     todofukenList = new ArrayList<Todofuken>();
 
-    } catch(Exception e){
+    //結果を取得
+    while(rs.next()){
+
+        Todofuken t = new Todofuken(
+            rs.getString(1),
+            rs.getString(2),
+            rs.getString(3)
+        );
+        todofukenList.add(t);
+        }
+        rs.close();
+        stmt.close();
+
+         } catch(Exception e){
        e.printStackTrace();
     } finally {
        try {
     //接続を閉じる
         conn.close();
     } catch(Exception e){
-
     }
   }
 }
 
-public int getUpdateRows(){
-    return updateRows;
+public ArrayList<Todofuken> getTodofukenList(){
+    return todofukenList;
     }
 }
